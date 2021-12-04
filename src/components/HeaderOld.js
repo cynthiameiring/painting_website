bde43546ac617cd173a75f517a9528690a6e870e
Cynthia Meiring <cynthia@zicht.nl>
2021-12-04 22:41:51 +0100

    Restyle header with new header image

---
+++ src/components/HeaderOld.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import headerImageLarge from "../media/HeaderImageLarge.jpg";
+import headerImageMedium from "../media/HeaderImageMedium.jpg";
+import headerImageSmall from "../media/HeaderImageSmall.jpg";
+import headerImageMobile from "../media/HeaderImageMobile.jpg";
+import { headerImageLoaded } from "../actions/menu";
+import { connect } from "react-redux";
+// import Fade from "react-reveal/Fade";
+// import logo from "../media/LogoBlack.png";
+// import { Link } from "react-router-dom";
+
+class HeaderOld extends Component {
+  state = { loaded: true };
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="u-relative">
+        <div className="u-absolute  _top--15  u-width--100  u-z--1  u-padding--l3  u-padding--l0@lg">
+          <div className="o-grid  o-grid--fixed  u-width--100">
+            <div className="o-grid__row">
+              <div className="o-grid__col  o-grid__col--6@xs">
+                {this.state.loaded ? (
+                  <div>
+                    {/* <Fade left> */}
+                    <h1
+                      style={{ color: "#212121" }}
+                      className="u-size--6  u-size--4@sm  u-size--3@md  u-size--2@lg  u-size--1@xl  u-weight--600  u-padding--l6@sm  _letter-spacing--title"
+                    >
+                      Photorealistic <br /> artworks
+                    </h1>
+                    <p
+                      style={{ fontFamily: "'Nothing You Could Do', cursive" }}
+                      className="u-hidden  u-block@lg  u-size--6  u-size--5@xl  u-weight--300  u-lh--5  u-padding--l6@sm  u-padding--t3"
+                    >
+                      by Cynthia Meiring
+                    </p>
+                    {/* </Fade> */}
+                  </div>
+                ) : null}
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className="u-hidden@sm  u-flex  u-flex--justify--center  u-padding--b5">
+          <div className="u-flex">
+            <img
+              src={logo}
+              className="u-margin--r4"
+              style={{ height: "80px" }}
+              alt="logo"
+            />
+            <p className="u-size--5">
+              CYNTHIA <br />
+              MEIRING
+            </p>
+          </div>
+        </div> */}
+        <picture>
+          <source media="(min-width: 1240px)" srcSet={headerImageLarge} />
+          <source media="(min-width: 990px)" srcSet={headerImageMedium} />
+          <source media="(min-width: 550px)" srcSet={headerImageSmall} />
+          <source media="(min-width: 0px)" srcSet={headerImageMobile} />
+          <img
+            srcSet={headerImageSmall}
+            alt="header"
+            className="u-width--100  u-block  u-z--0"
+            // onLoad={() => {
+            //   setTimeout(() => {
+            //     this.setState({ loaded: true });
+            //     this.props.headerImageLoaded(true);
+            //   }, 500);
+            // }}
+          />
+        </picture>
+      </div>
+    );
+  }
+}
+
+// const mapStateToProps = (state) => {
+//   return {
+//     headerImagedLoaded: state.loaded,
+//   };
+// };
+
+export default connect(null, {
+  headerImageLoaded,
+})(HeaderOld);
