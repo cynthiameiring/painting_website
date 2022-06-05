1a35f94017f012164082b48d94ad75bfe0345431
Cynthia Meiring <cynthia@zicht.nl>
2022-06-05 21:05:08 +0200

    Test connection to database with About-page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,22 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import IntroContentPage from "../components/IntroContentPage";
-import data from "../data.js";
+import superagent from "superagent";
+import {serverUrl} from "../constants";
 
 export default class About extends Component {
   state = {
-    about: data.pages.find((p) => p.title === "About"),
+    about: {},
   };
 
   componentDidMount() {
     window.scrollTo(0, 0);
+    superagent(`${serverUrl}/about`)
+      .then(response => {
+        this.setState({
+          about: response.body
+        });
+      })
+      .catch(console.error);
   }
 
   render() {
@@ -19,10 +27,11 @@ export default class About extends Component {
           intro={this.state.about.teaserText}
         />
         <div className="o-grid__row">
-          <div className="o-grid__col  o-grid__col--12@xs  o-grid--offset--2@sm  o-grid__col--8@sm  o-grid--offset--3@lg  o-grid__col--6@lg">
+          <div
+            className="o-grid__col  o-grid__col--12@xs  o-grid--offset--2@sm  o-grid__col--8@sm  o-grid--offset--3@lg  o-grid__col--6@lg">
             <img
-              src={this.state.about.teaserImage.url}
-              alt={this.state.about.teaserImage.alt}
+              src={this.state.about.teaserImageUrl}
+              alt={this.state.about.teaserImageAlt}
               className="u-relative  u-width--100"
             />
           </div>
